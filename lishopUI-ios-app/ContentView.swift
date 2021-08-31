81998f53670716589882b7e926320b08f86c93ab
Daniel Vela <veladan@me.com>
2021-08-31 19:40:47 +0200

    Initial Commit

---
+++ lishopUI-ios-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lishopUI-ios-app
+//
+//  Created by Daniel Vela on 31/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
